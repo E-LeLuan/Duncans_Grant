bc519559981cd54785a3330d5180b3d12c9e7e17
Elizabeth Le-luan <39098891+E-LeLuan@users.noreply.github.com>
2020-11-14 15:36:15 +0000

    batches created as two full batches will not extract

--- detect_error_script.R
+++ detect_error_script.R
@@ -35,52 +35,54 @@ FA_detect_script_error %>%
 
 # files with no error in script (correct)
 # read in data
-# FA_filenames_corr <- read_csv('FA_filenames_corr.csv', col_names = FALSE) #54 obs. 20 20 14
+FA_filenames_corr <- read_csv('FA_filenames_corr.csv', col_names = FALSE) 
+#54 obs. 20 20 14
 
 # assign batch numbers
-#batch_numbers_FA_corr <- c(rep("batch1", 20),
- # rep("batch2", 20),
-#  rep("batch3", 14))
+batch_numbers_FA_corr <- c(rep("batch1", 20),
+rep("batch2", 20),
+rep("batch3", 14))
 
 # add batch numbers as a column
-#FA_filenames_corr <- FA_filenames_corr %>%
- # cbind(batch_numbers_FA_corr)
+FA_filenames_corr <- FA_filenames_corr %>%
+cbind(batch_numbers_FA_corr)
 
 # write separate .csv files
-#FA_filenames_corr %>%
- # filter(batch_numbers_FA_corr == "batch1") %>%
-  #select(X1) %>%
-  #write_csv(path = 'FA_batch1_corr.csv', col_names = FALSE)
+FA_filenames_corr %>%
+filter(batch_numbers_FA_corr == "batch1") %>%
+select(X1) %>%
+write_csv(path = 'FA_batch1_corr.csv', col_names = FALSE)
 
-#FA_filenames_corr %>%
- # filter(batch_numbers_FA_corr == "batch2") %>%
-#  select(X1) %>%
-#  write_csv(path = 'FA_batch2_corr.csv', col_names = FALSE)
+FA_filenames_corr %>%
+filter(batch_numbers_FA_corr == "batch2") %>%
+select(X1) %>%
+write_csv(path = 'FA_batch2_corr.csv', col_names = FALSE)
 
-#FA_filenames_corr %>%
-#  filter(batch_numbers_FA_corr == "batch3") %>%
- # select(X1) %>%
-#  write_csv(path = 'FA_batch3_corr.csv', col_names = FALSE)
+FA_filenames_corr %>%
+filter(batch_numbers_FA_corr == "batch3") %>%
+select(X1) %>%
+write_csv(path = 'FA_batch3_corr.csv', col_names = FALSE)
 
 # files with error in script 
-# read in data
-# FA_filenames_error <- read_csv('FA_filenames_error.csv', col_names = FALSE) #36 obs. 20 16
+#read in data
+FA_filenames_error <- read_csv('FA_filenames_error.csv', col_names = FALSE) 
+#36 obs. 20 16
 
 # assign batch numbers
-# batch_numbers_FA_error <- c(rep("batch4", 20),
-  #                         rep("batch5", 16))
+batch_numbers_FA_error <- c(rep("batch4", 20),
+                      rep("batch5", 11))
 
 # add batch numbers as a column
-# FA_filenames_error <- FA_filenames_error %>%
-  #cbind(batch_numbers_FA_error)
+FA_filenames_error <- FA_filenames_error %>%
+  cbind(batch_numbers_FA_error)
 
 # write separate csv files
-#FA_filenames_error %>%
- # filter(batch_numbers_FA_error == "batch4") %>%
-  #select(X1) %>%
-  #write_csv(path = 'FA_batch4_error.csv', col_names = FALSE)
+FA_filenames_error %>%
+filter(batch_numbers_FA_error == "batch4") %>%
+select(X1) %>%
+write_csv(path = 'FA_batch4_error.csv', col_names = FALSE)
 
-#FA_filenames_error %>%
- # filter(batch_numbers_FA_error == "batch5") %>%
-#  select(X1) %>%
-#  write_csv(path = 'FA_batch5_error.csv', col_names = FALSE)
+FA_filenames_error %>%
+filter(batch_numbers_FA_error == "batch5") %>%
+select(X1) %>%
+write_csv(path = 'FA_batch5_error.csv', col_names = FALSE)
